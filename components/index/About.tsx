e19741a4aaeb41e1e3e971b9f1d414aed038b2ad
Serhii Dychko <dychkosergey@gmail.com>
2024-05-14 23:00:15 +0300

    update about section

--- components/index/About.tsx
+++ components/index/About.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useTranslations } from "next-intl";
 import Heading3 from "@/components/ui/typography/Heading3";
 import Body1 from "@/components/ui/typography/Body1";
+import Link from "next/link";
 
 const About: React.FC = () => {
   const t = useTranslations("About");
@@ -15,8 +16,8 @@ const About: React.FC = () => {
         <Heading>{t("heading")}</Heading>
 
         <div className="mt-12 flex flex-col items-center md:items-start md:flex-row gap-12 xl:gap-[192px]">
-          <div className="min-w-72 w-72 h-80 relative">
-            <div className="z-0 absolute w-72 h-72 bg-gray-400 top-5 left-5"></div>
+          <div className="min-w-80 w-72 h-80 relative">
+            <div className="z-0 absolute w-80 h-80 bg-gray-400 top-5 left-5"></div>
 
             <Image
               src="/images/img_me.jpg"
@@ -33,13 +34,40 @@ const About: React.FC = () => {
               {t("title")}
             </Heading3>
 
-            {Array(4)
+            {Array(3)
               .fill(false)
               .map((el, index) => (
                 <Body1 className="mb-4" key={index}>
                   {t(`body.${index}`)}
                 </Body1>
               ))}
+            <Body1 className="mb-4">{t("avocation")}</Body1>
+            <div className="flex flex-row gap-4 mb-4">
+              <Link
+                href="https://www.runtastic.com/user/Q59CQ2ZBAVRS3NZN"
+                className="p-2 rounded-lg bg-gray-200 dark:bg-gray-600 flex gap-2 items-center"
+              >
+                <Image
+                  src="/images/adidas-logo.svg"
+                  height={32}
+                  width={32}
+                  alt="Adidas Running"
+                />{" "}
+                {t("running")}
+              </Link>
+              <Link
+                href="https://hevy.com/user/dychkos"
+                className="p-2 rounded-lg bg-gray-200 dark:bg-gray-600 flex gap-2 items-center"
+              >
+                <img
+                  src="/images/hevy-logo.svg"
+                  height={32}
+                  width={32}
+                  alt="Hevy sport"
+                />{" "}
+                {t("sport")}
+              </Link>
+            </div>
             <Body1 className="mb-4">{t("summary")}</Body1>
             <ul className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 list-disc list-inside gap-x-2.5">
               {Array(4)
