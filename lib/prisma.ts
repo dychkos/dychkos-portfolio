3cbb31badd0b2e27bf3ee9aac0ba89d5be97b874
Serhii Dychko <dychkosergey@gmail.com>
2025-03-08 09:30:13 +0200

    update prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-prisma = new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+export default prisma
+
