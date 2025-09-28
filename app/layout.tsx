68dc299f854a3a148a9302c27496ac4cbfa0fad5
Serhii Dychko <dychkosergey@gmail.com>
2025-09-28 16:11:03 +0300

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,24 +12,8 @@ export default function RootLayout({ children }: Props) {
   return (
     <html>
       <head>
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='apple-mobile-web-app-title' content='Serhii D.' />
+        <title>Serhii D. Portfolio</title>
       </head>
       <body>
         {children}
